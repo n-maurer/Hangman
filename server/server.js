109bb36356cdbfbda6e8ae932c60ceec9bbbfb5c
Noah Maurer <noahmaurer12@gmail.com>
2023-05-08 09:56:14 -0500

    Word of day CRUD

--- server/server.js
+++ server/server.js
@@ -107,7 +107,7 @@ app.put("/api/categories/:id", async (request, response) => {
     } catch (err) {
         response.status(400).json({
             status: "error",
-            message: "Error updating new category",
+            message: "Error updating category",
         });
     }
 });
@@ -220,7 +220,155 @@ app.put("/api/words/:id", async (request, response) => {
     } catch (err) {
         response.status(400).json({
             status: "error",
-            message: "Error updating new category",
+            message: "Error updating word",
+        });
+    }
+});
+
+//Word of Day
+
+//Todays Date
+function getTodaysDate() {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (day < 10) {
+        day = "0" + day;
+    }
+    let formattedDate = year + "/" + month + "/" + day;
+    return formattedDate;
+}
+
+//List all Word of Day Table
+app.get("/api/word-of-day", async (request, response) => {
+    try {
+        const results = await db.query(
+            `SELECT wod.id, wod.date, wod.word_id, w.name AS word_name
+             FROM word_of_day wod
+             LEFT JOIN words w ON wod.word_id = w.id`
+        );
+        console.log(results);
+        response.status(200).json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                word_of_day: results.rows,
+            },
+        });
+    } catch (err) {
+        response.status(500).json({
+            status: "error",
+            message: "Error getting all words",
+        });
+    }
+});
+
+//Random Word from Words Table
+app.get("/api/word-of-day/random", async (request, response) => {
+    try {
+        const randomResults = await db.query(
+            `SELECT w.id, w.name, w.category_id, c.name AS category_name
+             FROM words w
+             LEFT JOIN categories c ON w.category_id = c.id
+             ORDER BY random()
+             LIMIT 1`
+        );
+        console.log(randomResults);
+        response.status(200).json({
+            status: "success",
+            results: randomResults.rows.length,
+            data: {
+                words: randomResults.rows,
+            },
+        });
+    } catch (err) {
+        response.status(500).json({
+            status: "error",
+            message: "Error getting random word",
+        });
+    }
+});
+
+//Create Word of Day
+//currently the data you send is just a word id (from word table)
+app.post("/api/word-of-day", async (request, response) => {
+    var today = getTodaysDate();
+    try {
+        const results = await db.query(
+            "INSERT INTO word_of_day (word_id, date) VALUES ($1,$2) returning *",
+            [request.body.word_id, today]
+        );
+        response.status(200).json({
+            status: "success",
+            data: {
+                word_of_day: results.rows[0],
+            },
+        });
+    } catch (err) {
+        response.status(400).json({
+            status: "error",
+            message: "Error creating new word of day",
+        });
+    }
+});
+
+//Delete Word of Day
+//By ID
+app.delete("/api/word-of-day/:id", async (request, response) => {
+    var wod_id = request.params.id;
+    try {
+        const results = await db.query(
+            "DELETE FROM word_of_day WHERE id = ($1)",
+            [wod_id]
+        );
+        response.status(200).json({
+            status: "success",
+        });
+    } catch (err) {
+        response.status(400).json({
+            status: "error",
+            message: "Error deleting word of day",
+        });
+    }
+});
+
+//Update Word of Day
+app.put("/api/word-of-day/:id", async (request, response) => {
+    var wod_id = request.params.id;
+    try {
+        const results = await db.query(
+            "UPDATE word_of_day SET word_id = $1 WHERE id = $2 RETURNING *",
+            [request.body.word_id, wod_id]
+        );
+        response.status(200).json({
+            status: "success",
+            data: {
+                words: results.rows[0],
+            },
+        });
+    } catch (err) {
+        response.status(400).json({
+            status: "error",
+            message: "Error updating word of day",
+        });
+    }
+});
+
+//Clear Word of Day
+app.delete("/api/word-of-day/table/clear", async (request, response) => {
+    try {
+        const results = await db.query("DELETE FROM word_of_day");
+        response.status(200).json({
+            status: "success",
+        });
+    } catch (err) {
+        response.status(400).json({
+            status: "error",
+            message: "Error clearing word of day table",
         });
     }
 });
