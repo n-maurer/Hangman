d505792a3e71e25b455f24d0da47c2b5e73b0452
Noah Maurer <noahmaurer12@gmail.com>
2023-05-09 11:44:29 -0500

    redux set up

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,24 @@ import CategoryContainer from "./CategoryContainer";
 import SharkContainer from "./SharkContainer";
 import WordProgressContainer from "./WordProgressContainer";
 import KeyboardContainer from "./KeyboardContainer";
+import { connect } from "react-redux";
+import {
+    increaseCounter,
+    decreaseCounter,
+} from "./redux/Counter/counter.actions";
 
-function App() {
+function App(props) {
     return (
         <div className="App">
+            <div>Count: {props.count}</div>
+
+            <button onClick={() => props.increaseCounter()}>
+                Increase Count
+            </button>
+
+            <button onClick={() => props.decreaseCounter()}>
+                Decrease Count
+            </button>
             <div className="game-container">
                 <HeaderContainer />
                 <AlphabetContainer />
@@ -22,4 +36,18 @@ function App() {
     );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        count: state.counter.count,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        increaseCounter: () => dispatch(increaseCounter()),
+
+        decreaseCounter: () => dispatch(decreaseCounter()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react";
-
-const store = createStore();
+import store from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
