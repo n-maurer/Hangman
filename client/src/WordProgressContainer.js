9503f45e6a0b45b69f858a3db158dec91a82e3fb
Noah Maurer <noahmaurer12@gmail.com>
2023-05-09 09:19:06 -0500

    Ready to implement backend

--- client/src/WordProgressContainer.js
+++ client/src/WordProgressContainer.js
@@ -1,5 +1,5 @@
 function WordProgressContainer() {
-    const word = "Spongebob Squarepants";
+    const word = "Lion King";
     const splitWord = word.split("");
 
     const currentProgress = [];
@@ -8,6 +8,27 @@ function WordProgressContainer() {
             currentProgress.push(" ");
         } else currentProgress.push("_");
     }
+
+    var currentLetter = "K";
+    for (let letter in splitWord) {
+        if (splitWord[letter].toLowerCase() === currentLetter.toLowerCase()) {
+            currentProgress[letter] = splitWord[letter];
+        }
+    }
+    var currentLetter2 = "L";
+    for (let letter in splitWord) {
+        if (splitWord[letter].toLowerCase() === currentLetter2.toLowerCase()) {
+            currentProgress[letter] = splitWord[letter];
+        }
+    }
+    var currentLetter3 = "I";
+    for (let letter in splitWord) {
+        if (splitWord[letter].toLowerCase() === currentLetter3.toLowerCase()) {
+            currentProgress[letter] = splitWord[letter];
+        }
+    }
+
+    console.log(currentProgress);
 
     return (
         <div className="stacked-div word-progress-container">
@@ -18,7 +39,17 @@ function WordProgressContainer() {
                             {letter === " " ? (
                                 <div className="new-line"></div>
                             ) : (
-                                <div className="letter-space"> </div>
+                                <>
+                                    {letter === "_" ? (
+                                        <div className="letter-space"> </div>
+                                    ) : (
+                                        <div className="letter-space">
+                                            <div className="letter-text">
+                                                {letter}
+                                            </div>
+                                        </div>
+                                    )}
+                                </>
                             )}
                         </>
                     );
