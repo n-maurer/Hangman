d052718839789376592ca7cd409b9e73cf238fa4
Noah Maurer <noahmaurer12@gmail.com>
2023-05-10 17:59:41 -0500

    date fixed in header container

--- client/src/HeaderContainer.js
+++ client/src/HeaderContainer.js
@@ -2,17 +2,13 @@ import { connect } from "react-redux";
 
 function HeaderContainer(props) {
     var date = props.wordOfDayDate;
-    const month = date.charAt(5) + date.charAt(6);
-    const day = date.charAt(8) + date.charAt(9);
-    const year = date.slice(0, 4);
-    const formattedDate = `${month}/${day}/${year}`;
 
     return (
         <div className="stacked-div header-container">
             {date === "loading" ? (
                 <div className="loading-circle"></div>
             ) : (
-                <div>{formattedDate}</div>
+                <div>{date}</div>
             )}
         </div>
     );
